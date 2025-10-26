9983d13da4a3609b8c0612b829c7ee069bd8ccb8
madnanch786 <madnanch2016@gmail.com>
2025-10-26 09:33:01 -0700

    ADDED: Search Box, Filter, and Banner slider image are added

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,3 +1,5 @@
+import 'package:e_commoice_flutting_frontend/screens/home/widgets/item_banner.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -12,31 +14,59 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // Leading logo icon (red-black theme)
         leading: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
-            decoration: BoxDecoration(
-              color: Colors.red,
-              borderRadius: BorderRadius.circular(8),
-            ),
-            child: const Icon(Icons.shopping_bag, color: Colors.white),
-          ),
+          padding: const EdgeInsets.all(10),
+          child: Image.asset('assets/images/logo.png'),
         ),
         title: const Text(
-          'eShopping',
+          'eShoping',
           style: TextStyle(
             color: Colors.black,
             fontSize: 22,
             fontWeight: FontWeight.bold,
           ),
         ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 15),
+            child: Badge(
+              label: Text('10'),
+              child: Icon(CupertinoIcons.bell, size: 28),
+            ),
+          ),
+        ],
         centerTitle: true,
         backgroundColor: Colors.white,
         elevation: 5,
       ),
-      body: const Center(
-        child: Text('Welcome to eShopping!', style: TextStyle(fontSize: 18)),
+      body: ListView(
+        children: [
+          // Search Box
+          Card(
+            margin: const EdgeInsets.all(15),
+            child: TextField(
+              decoration: const InputDecoration(
+                hintText: 'Search',
+                prefixIcon: Icon(CupertinoIcons.search),
+                suffixIcon: Icon(Icons.filter_list),
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.all(15),
+              ),
+            ),
+          ),
+
+          // Banner
+          SizedBox(
+            height: 150,
+            child: PageView(
+              children: [
+                ItemBanner(image: 'banner2.png'),
+                ItemBanner(image: 'banner1.png'),
+                ItemBanner(image: 'banner3.png'),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
